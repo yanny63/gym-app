6fa997a2703d20150b60dad30d318319d1195689
yanny63 <contact.yanny0@gmail.com>
2026-04-20 22:55:01 +0200

    feat: add new landing page animations

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible')
+            }
+        else {
+            entry.target.classList.remove('visible')
+        }
+        }
+    )
+}, {threshold: 0.1})
+
+document.querySelectorAll(".section").forEach(section => {
+    observer.observe(section)
+})
